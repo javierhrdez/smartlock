da6264d73c74b98cc621f9c513caeaba699a49ba
Javier Hernandez <javier.hrdez@gmail.com>
2017-11-23 21:59:00 -0600

    datos json

---
+++ data.js
@@ -0,0 +1,21 @@
+usuarios = [
+    {
+        "id" : "001",
+        "username": "oalcantara",
+        "password": "12345",
+        "first_name" : "Osvaldo",
+        "last_name" : "Alcantara",
+        "devices":[]
+    }
+]
+
+devices = [
+    {
+        "id":"001",
+        "owner": "oalcantara",
+        "status": true,
+        "name":"puerta1",
+        "usuarios": []
+
+    }
+]
